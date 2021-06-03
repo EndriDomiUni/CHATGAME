f59056c7015501b403b21ceb50ca5cc7afcb053c
Endri Domi <endri.domi@studio.unibo.it>
2021-06-03 20:47:58 +0200

    Update server.py

--- ChatGame/server.py
+++ ChatGame/server.py
@@ -28,10 +28,57 @@ def manage_client(client):
 
     while True:
         msg = client.recv(BUFSIZ)
+
         if msg != bytes("{quit}", "utf8"):
             broadcast(msg, name + ": ")
+
             if msg == bytes("{play}", "utf8"):
-                play()
+
+                actual_role = senteces.role[senteces.level]
+                welcome_game = 'Welcome to the game! Your actually role is %s, if you win: you rank up! ' \
+                               'I will propose you three questions, one will be a trick and you will lose the game ' \
+                               'immediately' \
+                               ', instead the other two contain a question' % actual_role
+
+                # mandare messaggio: welcome_home
+                client.send(bytes(welcome_game, "utf8"))
+
+                # poni domande
+                intro = "These are the three questions:"
+                client.send(bytes(intro, "utf8"))
+
+                quest1 = senteces.quest.get(random.randint(1, 8))
+                quest2 = senteces.quest.get(random.randint(1, 8))
+                trap = senteces.quest.get(random.randint(1, 8))
+
+                # stampa quesiti
+                client.send(bytes(quest1), "utf8")
+                client.send(bytes(quest2), "utf8")
+                client.send(bytes(trap), "utf8")
+
+                # ottieni risposta
+                answer = ""
+                # se la risposta è uguale a trap -> quit
+
+                # trap.value
+                if answer == trap:
+                    client.send(bytes("{quit}", "utf8"))
+                    client.close()
+                    del clients[client]
+                    broadcast(bytes("%s left the chat." % name, "utf8"))
+                    break
+                # se la risposta è giusta -> rank up
+                else:
+                    senteces.rank_up(actual_role)
+                    new_lever = "Your new role is %s" % actual_role
+                    client.send(new_lever, "utf8")
+
+                    # se il lvl è al max -> win
+                    if actual_role == senteces.role["king"]:
+                        win = "You have win %s" % name
+                        client.send(bytes(win, "utf8"))
+
+                # da capo
         else:
             client.send(bytes("{quit}", "utf8"))
             client.close()
@@ -43,32 +90,6 @@ def manage_client(client):
 def broadcast(msg, prefix=""):
     for user in clients:
         user.send(bytes(prefix, "utf8") + msg)
-
-
-def play():
-    actual_role = senteces.role[senteces.level]
-    welcome_game = 'Welcome to the game! Your actually role is %s, if you win: you rank up! ' \
-                   'I will propose you three questions, one will be a trick and you will lose the game immediately' \
-                   ', instead the other two contain a question' % actual_role
-
-    # mandare messaggio: welcome_home
-
-    # poni domande
-    quest1 = senteces.quest.get(random.randint(1, 8))
-    quest2 = senteces.quest.get(random.randint(1, 8))
-    trap = senteces.quest.get(random.randint(1, 8))
-
-    # stampa quesiti
-
-    # ottieni risposta
-
-    # se la risposta è uguale a trap -> quit
-    # se la risposta è giusta -> rank up
-
-    # se il lvl è al max -> win
-
-    # da capo
-
 
 
 clients = {}
